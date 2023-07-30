360536eb6a2fd2ed4055fae64da3f762e549b11d
AhmedAlaa <ahmedah37@gmail.com>
2023-07-31 00:04:54 +0300

    Calculator

---
+++ Calculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by Ahmed Alaa on 12/05/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
